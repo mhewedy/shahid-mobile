68dd9a6484ce9ba449faa9359c2fe332da4b6b57
Muhammad Hewedy <mhewedy@gmail.com>
2017-07-20 00:06:51 +0300

    update episode list view with watched after clicck the episode

--- src/pages/episode/episode.ts
+++ src/pages/episode/episode.ts
@@ -10,23 +10,14 @@ import { NavController, NavParams, ToastController } from 'ionic-angular';
 export class EpisodePage {
 
   items: Array<{id: number, videoUrl: string, durationSeconds: number, watched: boolean}>;
+  seriesId: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
      private episodeService: EpisodeService, private toastController: ToastController) {
 
     var navItem: any = navParams.get('item');
-
-    this.episodeService.listEpisode(navItem.id).subscribe(
-      data => {
-        this.items = data;
-        console.log(data);
-      },
-      err => {
-        this.showServerIpMissingToast(err)
-        console.log(err);
-      }, 
-      () => console.log('completed')
-    );
+    this.seriesId = navItem.id;
+    this.loadList();
   }
 
   itemTapped(event, item){
@@ -41,8 +32,22 @@ export class EpisodePage {
           }, 
           () => console.log('completed')
         );
-        
+      this.loadList();
       window.open(item.videoUrl, '_system');
+  }
+
+  private loadList(){
+    this.episodeService.listEpisode(this.seriesId).subscribe(
+      data => {
+        this.items = data;
+        console.log(data);
+      },
+      err => {
+        this.showServerIpMissingToast(err)
+        console.log(err);
+      }, 
+      () => console.log('completed')
+    );
   }
 
   private showServerIpMissingToast(err: Error){
